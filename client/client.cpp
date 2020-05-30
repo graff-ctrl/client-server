397524767f4b14529d70f5e6fff5262a57afb69d
graff-ctrl <agraff@seattleu.edu>
2020-05-29 17:35:34 -0700

    Added parsing from Mike's code to extract username and password.

--- client/client.cpp
+++ client/client.cpp
@@ -7,8 +7,87 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
 #define PORT 12008
+class KeyValue
+{
+private:
+    char m_szKey[128];
+    char m_szValue[2048];
 
+public:
+
+    KeyValue() {};
+    void setKeyValue(char *pszBuff)
+    {
+        char *pch1;
+
+        // find out where the "=" sign is, and take everything to the left of the equal for the key
+        // go one beyond the = sign, and take everything else
+        pch1 = strchr(pszBuff, '=');
+        assert(pch1);
+        int keyLen = (int)(pch1 - pszBuff);
+        strncpy(m_szKey, pszBuff, keyLen);
+        m_szKey[keyLen] = 0;
+        strcpy(m_szValue, pszBuff + keyLen + 1);
+    }
+
+    char *getKey()
+    {
+        return m_szKey;
+    }
+
+    char *getValue()
+    {
+
+        return m_szValue;
+    }
+
+};
+class RawKeyValueString
+{
+private:
+
+    char m_szRawString[32768];
+    int m_currentPosition;
+    KeyValue *m_pKeyValue;
+    char *m_pch;
+public:
+
+    RawKeyValueString(char *szUnformattedString)
+    {
+        assert(strlen(szUnformattedString));
+        strcpy(m_szRawString, szUnformattedString);
+
+        m_pch = m_szRawString;
+
+    }
+    ~RawKeyValueString()
+    {
+        if (m_pKeyValue)
+            delete (m_pKeyValue);
+    }
+
+    void getNextKeyValue(KeyValue & keyVal)
+    {
+        // It will attempt to parse out part of the string all the way up to the ";", it will then create a new KeyValue object  with that partial string
+        // If it can;t it will return null;
+        char *pch1;
+        char szTemp[32768];
+
+        pch1 = strchr(m_pch, ';');
+        assert(pch1 != NULL);
+        int subStringSize = (int)(pch1 - m_pch);
+        strncpy(szTemp, m_pch, subStringSize);
+        szTemp[subStringSize] = 0;
+        m_pch = pch1 + 1;
+        if (m_pKeyValue)
+            delete (m_pKeyValue);
+        keyVal.setKeyValue(szTemp);
+
+    }
+
+};
 //Funtion to connect to server.
 int connectRPC(char *userName, char *password)
 {
@@ -16,9 +95,6 @@ int connectRPC(char *userName, char *password)
     // username
     // password
     // input format="rpc=connect;username=<Your user>;password=<Your password>;"
-    char connect[50];
-    strcpy(connect, "rpc=connect;username="+userName+";password="+password);
-    printf(connect);
     // Output arguments are:
     // status     (This will be set to 1 if success and -1 if error)
     // error      ( This will be to blank or an error message)
@@ -99,13 +175,56 @@ int disconnectServer(int sock)
     status = close(sock);
     return status;
 }
+int Connect(char *pszUserName, char *pszPass)
+{
+    printf("Username = %s  Password = %s", pszUserName, pszPass);
+    return 0;
+}
+int processRPC(char *szTest1){
+    // Create a couple of buffers, and see if works
+    //const char *szTest1 = "rpc=connect;user=mike;password=123;";
+    RawKeyValueString *pRawKey = new RawKeyValueString((char *)szTest1);
+    KeyValue rpcKeyValue;
+    char *pszRpcKey;
+    char *pszRpcValue;
 
+    // Figure out which rpc it is
 
+    pRawKey->getNextKeyValue(rpcKeyValue);
+    pszRpcKey = rpcKeyValue.getKey();
+    pszRpcValue = rpcKeyValue.getValue();
+
+    if (strcmp(pszRpcKey, "rpc") == 0)
+    {
+        if (strcmp(pszRpcValue, "connect") == 0)
+        {
+            // Get the next two arguments (user and password);
+            KeyValue userKeyValue;
+            KeyValue passKeyValue;
+
+            char *pszUserKey;
+            char *pszUserValue;
+            char *pszPassKey;
+            char *pszPassValue;
+            int status;
+
+            pRawKey->getNextKeyValue(userKeyValue);
+            pszUserKey = userKeyValue.getKey();
+            pszUserValue = userKeyValue.getValue();
+
+            pRawKey->getNextKeyValue(passKeyValue);
+            pszPassKey = passKeyValue.getKey();
+            pszPassValue = passKeyValue.getValue();
+
+            status = Connect(pszUserValue, pszPassValue);
+        }
+    }
+}
 int main(int argc, char const *argv[])
 {
-    char username = "usernametest";
-    char password = "passwordtest";
-    connectRPC(username,password);
+    const char *szTest1 = "rpc=connect;user=mike;password=123;";
+    int status;
+    status = processRPC((char *)szTest1);
 
     /*int sock = 0;
     int status;
